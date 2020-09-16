255420f8d20b76816f91c694947b60b5b3cece68
Alex Niarchos <alexandros.niarchos@theblueground.com>
2020-09-16 19:57:29 +0300

    Add deploy branch option in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ set -x
 # deploy server
 cd ./server
 git fetch
+if [ ! -z $1 ]
+then
+    git checkout $1
+fi
 git pull --rebase
 npm i
 pm2 restart server
@@ -11,6 +15,10 @@ pm2 restart server
 #deploy client
 cd /var/www/planning-poker.alexniarchos.com/planningpoker/client
 git fetch
+if [ ! -z $1 ]
+then
+    git checkout $1
+fi
 git pull --rebase
 npm i
 npm run build
